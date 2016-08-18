cab870fe4bc17e39159cbc33b4387e93a82fdfd9
Rodrigo González <rodrigog.84@gmail.com>
2016-08-18 12:16:38 -0300

    agrega largo limite en detalle  de factura glosa

--- core/application/controllers/facturaglosa.php
+++ core/application/controllers/facturaglosa.php
@@ -155,7 +155,7 @@ class Facturaglosa extends CI_Controller {
 			$lista_detalle = array();
 			$i = 0;
 			foreach ($detalle_factura as $detalle) {
-				$lista_detalle[$i]['NmbItem'] = $detalle->glosa;
+				$lista_detalle[$i]['NmbItem'] = substr($detalle->glosa,0,70);
 				$lista_detalle[$i]['QtyItem'] = 1;
 				//$lista_detalle[$i]['PrcItem'] = $detalle->precio;
 				//$lista_detalle[$i]['PrcItem'] = round((($detalle->precio*$detalle->cantidad)/1.19)/$detalle->cantidad,0);
