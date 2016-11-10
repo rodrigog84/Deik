8b1f8d36af9d7a12c0da287a74618ee34714e65f
Rodrigo González <rodrigog.84@gmail.com>
2016-11-10 17:22:39 -0300

    comienza analisis de rehacer cuentas corrientes

--- core/application/controllers/procesos.php
+++ core/application/controllers/procesos.php
@@ -21,6 +21,41 @@ class Procesos extends CI_Controller {
 	}
 
 
+	public function genera_cuentas_corrientes(){
+
+
+			/*
+
+
+truncate cartola_cuenta_corriente;
+truncate cuenta_corriente;
+truncate detalle_cuenta_corriente;
+truncate detalle_mov_cuenta_corriente;
+truncate movimiento_cuenta_corriente;
+
+##SUMAR FACTURAS SEGÚN PREVENTAS
+##SUMAR NOTAS DE DEBITO
+#DESCONTAR SEGÚN RECAUDACIONES
+#DESCONTAR SEGÚN NOTAS DE CRÉDITO
+
+
+
+				NECESITO:
+
+				$idcliente
+				$ftotal: totalfactura
+				$fechafactura: fecha
+				$tipodocumento : 
+				$numfactura
+				$fechavenc: fecha_venc
+
+
+			*/
+
+
+	}	
+
+
 }
 
 
